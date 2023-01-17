e58e21abda473e337c392f6af11aed7d590b0fe7
2123021 <122844877+2123021@users.noreply.github.com>
2023-01-17 04:54:41 +0000

    1st

--- app.py
+++ app.py
@@ -1,39 +1,16 @@
-#「最適化問題のシミュレータ」アプリを作成するためのサンプルコード
 import streamlit as st
-import numpy as np
-from scipy.optimize import minimize
-import matplotlib.pyplot as plt
 
-st.title("Optimization Problem Simulator")
+# ウィンドウのタイトルを設定
+st.title("Number Comparison App")
 
-# Define the objective function
-def objective_function(x):
-    return x[0]**2 + x[1]**2
+# ユーザーに2つの整数を入力するように促す
+num1 = st.number_input("Enter a number:")
+num2 = st.number_input("Enter another number:")
 
-# Define the constraints
-def constraint1(x):
-    return x[0] + x[1] - 1
-
-def constraint2(x):
-    return 1 - x[0] - x[1]
-
-# Set the optimization bounds
-bounds = [(0, 1), (0, 1)]
-
-# Set the initial guess
-x0 = [0.5, 0.5]
-
-# Run the optimization
-result = minimize(objective_function, x0, bounds=bounds, constraints={"type": "eq", "fun": constraint1},
-                  constraints={"type": "ineq", "fun": constraint2})
-
-# Plot the results
-x = np.linspace(0, 1, 100)
-y = np.linspace(0, 1, 100)
-X, Y = np.meshgrid(x, y)
-Z = X**2 + Y**2
-
-plt.contour(X, Y, Z, levels=np.logspace(0, 2, 20))
-plt.scatter(result.x[0], result.x[1])
-
-st.pyplot()
+# 2つの整数を比較し、結果を表示する
+if num1 > num2:
+    st.success("The first number is greater.")
+elif num1 < num2:
+    st.success("The second number is greater.")
+else:
+    st.success("The two numbers are equal.")
